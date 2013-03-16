99120463571e81a88a7c24c83c2b877feac57b31
Janos Gyerik <janos@axiom>
2013-03-16 21:18:20 +0100

    added full version

---
+++ happy-moments-full/assets/sql_create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "happymoments_happymoment" (
+    "_id" integer NULL PRIMARY KEY AUTOINCREMENT,
+    "text" text NULL,
+    "filename" varchar(80) NULL,
+    "color" varchar(80) NULL,
+    "level" integer NULL,
+    "latitude" real NULL,
+    "longitude" real NULL,
+    "speed" real NULL,
+    "is_active" bool NULL,
+    "created_dt" datetime NULL,
+    "updated_dt" datetime NULL
+)
+;
