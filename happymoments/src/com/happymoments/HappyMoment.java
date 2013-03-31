bfea53a41e637f3daa48c8a3c15d000242eb069b
Janos Gyerik <janos@axiom>
2013-03-31 01:21:08 +0100

    added getters

--- happymoments/src/com/happymoments/HappyMoment.java
+++ happymoments/src/com/happymoments/HappyMoment.java
@@ -14,32 +14,44 @@ public class HappyMoment {
 		this.id = id;
 	}
 
+	public String getId() {
+		return id;
+	}
+
 	public void setText(String text) {
 		this.text = text;
+	}
+
+	public String getText() {
+		return text;
 	}
 
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
 
+	public String getFilename() {
+		return filename;
+	}
+
 	public void setColor(String color) {
 		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
 	}
 
 	public void setCreatedDate(long millis) {
 		this.createdDate = new Date(millis);
 	}
-	
-	public String toString() {
-		return String.format("text=%s date=%s", text, createdDate);
-	}
 
-	public String getText() {
-		return text;
-	}
-	
 	public Date getCreatedDate() {
 		return createdDate;
+	}
+
+	public String toString() {
+		return String.format("id=%s text=%s date=%s", id, text, createdDate);
 	}
 
 }
